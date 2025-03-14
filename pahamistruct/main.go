9bf6752d0293265d76614adaf5ce807562b0571c
ikuratugas <ikrartugas@gmail.com>
2025-03-14 23:00:58 +0800

    struct pelajaran ke-dua

--- pahamistruct/main.go
+++ pahamistruct/main.go
@@ -7,10 +7,15 @@ type Mahasisawa struct {
 	nim  string
 }
 
+func sebutMahasiswa(mhs Mahasisawa) {
+	fmt.Printf("MAHASISWA TEKNIK INFORMATIKA\n")
+	fmt.Printf("nama %s\n", mhs.nama)
+	fmt.Printf("nim %s\n", mhs.nim)
+}
+
 func main() {
 	fmt.Println("mencoba lagi")
 	baru := Mahasisawa{nama: "ikrar aprianto", nim: "20 650 028"}
-	fmt.Printf("MAHASISWA TEKNIK INFORMATIKA\n")
-	fmt.Printf("nama %s\n", baru.nama)
-	fmt.Printf("nim %s\n", baru.nim)
+
+	sebutMahasiswa(baru)
 }
