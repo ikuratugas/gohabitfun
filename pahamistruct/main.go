dbbe3390815e13de6f56574f0a676a1c7f56efee
ikuratugas <ikrartugas@gmail.com>
2025-03-16 14:19:44 +0800

    struct dasar hinnga http mission completed (basic)

--- pahamistruct/main.go
+++ pahamistruct/main.go
@@ -1,35 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
 
 type Mahasiswa struct {
-	nama string
-	nim  string
+	Nama string `json:"nama mahasiswa"`
+	Nim  string `json:"nim mahassiwa"`
 }
 
-func sapaMahasiswa(m *Mahasiswa) {
-	fmt.Printf("halo, %s di alamat %p\n", m.nama, &m.nama)
-}
+// func sapaMahasiswa(m *Mahasiswa) {
+// 	fmt.Printf("halo, %s di alamat %p\n", m.nama, &m.nama)
+// }
 
-func (m *Mahasiswa) ubahNamaMahasiswa(namaBaru string) {
-	m.nama = namaBaru
-	fmt.Printf("nama diperbarui jadi %s di alamat %p\n", m.nama, &m.nama)
-}
+// func (m *Mahasiswa) ubahNamaMahasiswa(namaBaru string) {
+// 	m.nama = namaBaru
+// 	fmt.Printf("nama diperbarui jadi %s di alamat %p\n", m.nama, &m.nama)
+// }
 
-func sebutMahasiswa(mhs Mahasiswa) {
-	fmt.Printf("MAHASISWA TEKNIK INFORMATIKA\n")
-	fmt.Printf("nama %s\n", mhs.nama)
-	fmt.Printf("nim %s\n", mhs.nim)
+// func sebutMahasiswa(mhs Mahasiswa) {
+// 	fmt.Printf("MAHASISWA TEKNIK INFORMATIKA\n")
+// 	fmt.Printf("nama %s\n", mhs.nama)
+// 	fmt.Printf("nim %s\n", mhs.nim)
+// }
+
+func showToJson(w http.ResponseWriter, r *http.Request) {
+	mahasiswa1 := Mahasiswa{
+		Nama: "ikrar aprianto",
+		Nim:  "2065028",
+	}
+
+	jsonData, err := json.Marshal(mahasiswa1)
+	if err != nil {
+		http.Error(w, "Gagal Mengubah Data", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonData)
 }
 
 func main() {
-	fmt.Println("mencoba lagi")
-	baru := Mahasiswa{nama: "ikrar aprianto", nim: "20 650 028"}
-	fmt.Printf("nilai alamat awal %p\n", &baru.nama)
-	sapaMahasiswa(&baru)
+	mux := http.NewServeMux()
 
-	fmt.Println("--")
-	baru.ubahNamaMahasiswa("rikar")
-
-	sebutMahasiswa(baru)
+	mux.HandleFunc("/", showToJson)
+	fmt.Println("sistem berjalan di server localhsot:8000")
+	http.ListenAndServe(":8000", mux)
 }
